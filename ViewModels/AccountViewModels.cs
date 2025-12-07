cc164d451842cf1a2f8491168a8c581a8d47c741
João Fernandes <156371666+Joshy180696@users.noreply.github.com>
2025-12-07 17:29:48 +0000

    Nova pagina account

---
+++ ViewModels/AccountViewModels.cs
@@ -0,0 +1,29 @@
+﻿namespace SmartBalanceBlazor.ViewModels
+{
+    public class AccountViewModels
+    {
+        public class BaseResponse
+        {
+            public bool Success { get; set; }
+            public string? ErrorMessage { get; set; }
+            public string? SuccessMessage { get; set; }
+        }
+
+        public class AccountInfo
+        {
+            public decimal MonthlyIncome { get; set; }
+            public string? Bio { get; set; }
+            public string FirstName { get; set; } = string.Empty;
+            public string LastName { get; set; } = string.Empty;
+            public string Address { get; set; } = string.Empty;
+            public string ContactNumber { get; set; } = string.Empty;
+        }
+
+        public class AccountResponse : BaseResponse
+        {
+            public AccountInfo? UserAccount { get; set; }
+        }
+
+       
+    }
+}
