a48aaafaceafa409dee6e3420b0a4a3b2b4f474e
João Fernandes <156371666+Joshy180696@users.noreply.github.com>
2025-12-07 22:46:54 +0000

    Funcionalidade de editar account concluida

--- ViewModels/AccountViewModels.cs
+++ ViewModels/AccountViewModels.cs
@@ -1,4 +1,6 @@
-﻿namespace SmartBalanceBlazor.ViewModels
+﻿using static SmartBalanceBlazor.ViewModels.AccountViewModels;
+
+namespace SmartBalanceBlazor.ViewModels
 {
     public class AccountViewModels
     {
@@ -9,6 +11,7 @@
             public string? SuccessMessage { get; set; }
         }
 
+        #region Fetch account
         public class AccountInfo
         {
             public decimal MonthlyIncome { get; set; }
@@ -18,12 +21,29 @@
             public string Address { get; set; } = string.Empty;
             public string ContactNumber { get; set; } = string.Empty;
         }
-
         public class AccountResponse : BaseResponse
         {
             public AccountInfo? UserAccount { get; set; }
         }
 
-       
+        #endregion
+
+        #region Update account 
+
+        public class UpdatedAccountInfo
+        {
+            public decimal? MonthlyIncome { get; set; }
+            public string? Bio { get; set; }
+            public string? Address { get; set; }
+            public string? ContactNumber { get; set; }
+        }
+
+        public class UpdatedAccountResponse : BaseResponse
+        {
+            public UpdatedAccountInfo? UpdatedAccountInfo { get; set; }
+        }
+        #endregion
+
+
     }
 }
