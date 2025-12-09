abe6ab8d01f48640e6febc12b76756a8a4428b34
João Fernandes <156371666+Joshy180696@users.noreply.github.com>
2025-12-09 19:32:21 +0000

    New page for expenses overview

--- ViewModels/Dashboard/DashboardResponse.cs
+++ ViewModels/Dashboard/DashboardResponse.cs
@@ -3,24 +3,49 @@ using static SmartBalanceBlazor.ViewModels.AccountViewModels;
 
 namespace SmartBalanceBlazor.ViewModels.Dashboard
 {
+
+    #region Financial Overview
     public class DashboardResponse : BaseResponse
     {
         public List<MonthlyExpensePoint>? MonthlyExpenses { get; set; }
         public List<AnnualExpensePoint>? AnnualExpenses { get; set; }
     }
-
     public class MonthlyExpensePoint
     {
-        public int Month { get; set; }  
+        public int Month { get; set; }
         public decimal Total { get; set; }
         public string MonthName =>
-        CultureInfo.CurrentCulture.DateTimeFormat.GetAbbreviatedMonthName(Month);
+            CultureInfo.CurrentCulture.DateTimeFormat.GetAbbreviatedMonthName(Month);
     }
-
     public class AnnualExpensePoint
     {
         public int Year { get; set; }
         public decimal Total { get; set; }
     }
+
+    #endregion
+
+    #region Expenses Overview
+
+    public class TotalExpenses
+    {
+        public string Description { get; set; } = string.Empty;
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public decimal Total { get; set; }
+        public string MonthName =>
+            CultureInfo.CurrentCulture.DateTimeFormat.GetAbbreviatedMonthName(Month);
+    }
+
+    public class DashboardExpensesResponse : BaseResponse
+    {
+        public bool HasData { get; set; }
+
+        public List<TotalExpenses>? Expenses { get; set; }
+
+    }
+
+    #endregion
 
 }
